d9d8a4403b57fcea70c7f0c05772209119aaa616
ipsitacn <ipsita.mallick@capitalnumbers.com>
2021-05-19 22:53:14 +0530

    test and code non zero words

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -26,14 +26,33 @@ describe("if there are no words guessed", () => {
   beforeEach(() => {
     wrapper = setup({ guessedWords: [] });
   });
-  test("renders without error", () => {
-    const component = findByTestArr(wrapper, "component-guessed-words");
-    expect(component.length).toBe(1);
-  });
+  test("renders without error", () => {});
   test("renders instructions to guess a word", () => {
     const instructions = findByTestArr(wrapper, "guess-instructions");
     expect(instructions.text().length).not.toBe(0);
   });
 });
 
-describe("if there are words guessed", () => {});
+describe("if there are words guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "agile", letterMatchCount: 1 },
+    { guessedWord: "party", letterMatchCount: 5 },
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test("renders without error", () => {
+    const component = findByTestArr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  test("renders guessed words section", () => {
+    const guessedWordsNode = findByTestArr(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1);
+  });
+  test("correct number of guessed words", () => {
+    const guessedWordNode = findByTestArr(wrapper, "guessed-word");
+    expect(guessedWordNode.length).toBe(guessedWords.length);
+  });
+});
