25e55eb4f2291418b5f8aacce9b2a1556b074f76
ipsitacn <ipsita.mallick@capitalnumbers.com>
2021-05-20 15:36:48 +0530

    add correct guess

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
 import axios from 'axios';
+
+export const actionTypes = {
+    CORRECT_GUESS: 'CORRECT_GUESS',
+}
+
+export function correctGuess() {
+    return { type: actionTypes.CORRECT_GUESS };
+}
+
 export const getSecretWord = () => {
     return axios.get('https://localhost:3030').then(res => res.data);
 }
