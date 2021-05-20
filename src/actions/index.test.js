8b540ecf323d977e0649963dfd5efd7cc15746ee
ipsitacn <ipsita.mallick@capitalnumbers.com>
2021-05-20 19:15:56 +0530

    action creators added

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,13 +1,5 @@
 import moxios from 'moxios';
-import { getSecretWord, correctGuess, actionTypes } from './index';
-
-
-describe('correctGuess', () => {
-    test('returns an action with type CORRECT_GUESS', () => {
-        const action = correctGuess();
-        expect(action).toStrictEqual({ type: actionTypes.CORRECT_GUESS });
-    });
-})
+import { getSecretWord } from './index';
 
 describe('getSecretWord', () => {
     beforeEach(() => {
